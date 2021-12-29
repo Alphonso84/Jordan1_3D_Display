8d56f2b2e337de2ab9cc8bab31e07474dda212f7
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-12-29 12:37:58 -0800

    Initial commit

---
+++ Assets/Scripts/RotateSneaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotateSneaker : MonoBehaviour
+{
+    public Vector3 editRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (Input.GetKey("r"))
+            {
+                transform.Rotate(editRotation * Time.deltaTime);
+                Debug.Log("Update");
+            }
+        
+
+    }
+}
